b5d3f0a8793a9d1826169a5e89c5969839f3d3b5
hermshermsherms <jeremy.staub94@gmail.com>
2016-08-31 18:17:27 -0400

    new storage map, waiting for changes

--- PleaseGod/src/StorageMap.java
+++ PleaseGod/src/StorageMap.java
@@ -6,25 +6,34 @@ import java.util.Hashtable;
 import java.util.Map;
 
 public class StorageMap{
-	protected static Hashtable<String, Stock> table; //issue with concurrent access
+	
+	 static Hashtable<String, Stock> table = new Hashtable<String, Stock>();
 	
 public Hashtable<String,Stock> createMap(){ //constructor of empty table
-	table = new Hashtable<String, Stock>();
+	Hashtable table = new Hashtable<String, Stock>();
 	return table;
 }
 
 public static void addToMap(String name, Stock stock ){
-	 //if (table.containsKey(name))
-	     // throw new IllegalArgumentException(
-	        //  "Cannot create new vertex with existing name.");
+	if(table.containsKey(name) || table.containsKey(stock)){
+		System.out.println("The system already contains" + name);
+		return;
+	}
 	table.put(name, stock);
-	
 }
+
 //testing
 //this is the worst
 public Stock getFromMap(String name){
+	if(table.containsKey(name)){
 	Stock rval = table.get(name);
 	return rval;
+	}
+	else{
+		//call pull from URL and return newStock 
+		addToMap(String name, Stock newStock);
+		getFromMap(name);
+	}
 }
 		
 
